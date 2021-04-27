f4c1270e74ff000c94c13479a7d56034455ef5d5
Karen Garcia <saykaren@gmail.com>
2021-04-26 21:12:50 -0600

    Switched to Rick and Morty

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,37 +1,20 @@
 import React, { useState } from "react";
 import "../Styling/App.css";
-import Navbar from "./NavBar";
-import Planets from "./Planets";
-import People from "./People";
-import {   useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,} from 'react-query'
-import {ReactQueryDevtools} from 'react-query-devtools';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+import RickAndMorty from "./RickAndMorty";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-function App() {
-  const [page, setPage] = useState("planets");
-
+const App = () => {
   return (
-    <>
     <QueryClientProvider client={queryClient}>
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage}/>
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
-      </div>
+      <div className="App">
+        <RickAndMorty />
       </div>
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
-      
-       </>
   );
-}
-
-
+};
 
 export default App;
