8f762dc106ed303d3afebf8ea6f713edd1d41f23
Karen Garcia <saykaren@gmail.com>
2021-04-28 21:15:16 -0600

    Moved Fetching in Main.js Moved fetching so I can pass data to my other components. Need help figuring out how to reference the cache for specific users

--- src/Components/App/Cards/CharacterCard.js
+++ src/Components/App/Cards/CharacterCard.js
@@ -1,18 +1,13 @@
-import React from 'react';
+import React from "react";
 
+const CharacterCard = ({ dataPoint, dataIndex }) => {
+  return (
+    <div key={dataIndex} className="card">
+      <h3>Name: {dataPoint.name}</h3>
+      <img src={dataPoint.image} alt={dataPoint.name} />
+      <p>Origin: {dataPoint.origin.name}</p> <p>Status: {dataPoint.status}</p>
+    </div>
+  );
+};
 
-
-const CharacterCard = ({dataPoint, dataIndex}) => {
-    return ( 
-        
-            <div key={dataIndex} className="card">
-              <h3>Name: {dataPoint.name}</h3>
-              <img src={dataPoint.image} />
-              <p>Origin: {dataPoint.origin.name}</p>{" "}
-              <p>Status: {dataPoint.status}</p>
-            </div>
-         
-     );
-}
- 
 export default CharacterCard;
