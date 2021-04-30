44f0ab23374cdf989b6bfb5c9dd2a5bf06a4adcc
Karen Garcia <saykaren@gmail.com>
2021-04-29 21:48:24 -0600

    Feorgot to save

--- src/Components/App/Episodes.js
+++ src/Components/App/Episodes.js
@@ -4,24 +4,24 @@ const EpisodesRicky = ({ characterList, locationList, episodeList }) => {
   const { data, status } = episodeList;
 
   return (
-    <> <h2>Episodes</h2>
-    <div className="card_section">
-     
-
-      {status === "error" && <div>Error Fetching Data</div>}
-      {status === "Loading" && <div>Loading Data....</div>}
-      {status === "success" && (
-        <>
-          {data.results.map((dataPoint, dataIndex) => (
-            <div key={dataIndex} className="card">
-              <h3>Episode Name: {dataPoint.name}</h3>
-              <p>Episode Number: {dataPoint.episode}</p>{" "}
-              <p>Characters: eventually pull here</p>
-            </div>
-          ))}
-        </>
-      )}
-    </div>
+    <>
+      {" "}
+      <h2>Episodes</h2>
+      <div className="card_section">
+        {status === "error" && <div>Error Fetching Data</div>}
+        {status === "Loading" && <div>Loading Data....</div>}
+        {status === "success" && (
+          <>
+            {data.results.map((dataPoint, dataIndex) => (
+              <div key={dataIndex} className="card">
+                <h3>Episode Name: {dataPoint.name}</h3>
+                <p>Episode Number: {dataPoint.episode}</p>{" "}
+                <p>Characters: eventually pull here</p>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </>
   );
 };
