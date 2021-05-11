95d87c95db217856efa789595e49248780e23bb0
Karen Garcia <saykaren@gmail.com>
2021-05-10 20:52:28 -0600

    Removal Of Unnecessary Items Removed the duplication of fetches and other components I no longer needed

--- src/Components/App/useQuery/fetchEpisodes.js
+++
@@ -1,7 +0,0 @@
-const fetchEpisodes = async ({ queryKey }) => {
-  const [, id] = queryKey;
-  const res = await fetch(`${id}`);
-  return res.json();
-};
-
-export default fetchEpisodes;
--- src/Components/App/useQuery/fetchLocations.js
+++
@@ -1,7 +0,0 @@
-const fetchLocations = async ({ queryKey }) => {
-  const [, id] = queryKey;
-  const res = await fetch(`${id}`);
-  return res.json();
-};
-
-export default fetchLocations;
